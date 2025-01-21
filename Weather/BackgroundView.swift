aa57a4cc4c7804da9678376841eec1d330d5c284
MAC IOS <magico7500@gmail.com>
2025-01-21 08:59:59 +0100

    hehe

---
+++ Weather/BackgroundView.swift
@@ -0,0 +1,18 @@
+//
+//  BackgroundView.swift
+//  Weather
+//
+//  Created by froehly jean-baptiste on 21/01/2025.
+//
+
+import SwiftUI
+
+struct BackgroundView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BackgroundView()
+}
---
+++ Weather/Temperature.swift
@@ -0,0 +1,8 @@
+//
+//  Temperature.swift
+//  Weather
+//
+//  Created by froehly jean-baptiste on 21/01/2025.
+//
+
+import Foundation
