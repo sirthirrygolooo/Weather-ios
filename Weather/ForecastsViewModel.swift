fed84287b84e7216ae226c11aef3fc4f29a79360
jb_frhl <72656353+sirthirrygolooo@users.noreply.github.com>
2025-01-28 08:30:44 +0100

    Update ForecastsViewModel.swift

--- Weather/ForecastsViewModel.swift
+++ Weather/ForecastsViewModel.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+
+class ForecastsViewModel: ObservableObject {
+    //modification de l'attribut en public
+    @Published var forecasts: [Temperature] = []
+    
+    init() {
+        getForecasts()
+    }
+    
+    func getForecasts() {
+        var newForecasts: [Temperature] = [
+            Temperature(dayOfWeek: "MON", imageName: "cloud.sun.fill", temp: 10),
+            Temperature(dayOfWeek: "TUE", imageName: "cloud.sun.fill", temp: 20),
+            Temperature(dayOfWeek: "WED", imageName: "cloud.sun.fill", temp: 5),
+            Temperature(dayOfWeek: "THU", imageName: "cloud.sun.fill", temp: 26),
+            Temperature(dayOfWeek: "FRI", imageName: "cloud.sun.fill", temp: 17)
+        ]
+        
+        self.forecasts += newForecasts
+    }
+    
+    func reloadTemperature() {
+        for i in 0..<forecasts.count {
+            forecasts[i].temp += 1
+        }
+    }
+}
